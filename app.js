5876d01c49c747b9dd1709916e5b61c0114f4312
Jesse Meirink <jesse@meirink.com>
2022-05-28 16:49:41 -0500

    create getGuess method to accept a number between 1 and 100 and return it

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  },
+  getGuess: function () {
+    let guess = prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}`)
+    let parsedGuess = parseInt(guess)
+
+      if(parsedGuess >= game.smallestNum && parsedGuess <= game.biggestNum) {
+      console.log(parsedGuess)
+      return parsedGuess
+    }
   }
 }
+
+game.getGuess()
+
