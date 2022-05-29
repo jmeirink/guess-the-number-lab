09cdb9ce243c777099c8cf3e726678125fcaf9f5
Jesse Meirink <jesse@meirink.com>
2022-05-29 18:49:24 -0500

    fix render function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
+  biggestNum: 10, // Change this number back to 100 after testing
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
@@ -10,19 +10,27 @@ const game = {
 
       do {
         this.prevGuesses.push(this.getGuess())
-        console.log(this.prevGuesses)
+        this.render(this.getGuess(), this.secretNum, this.prevGuesses.length)
       } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
-
   },
-  getGuess: function () {
+  getGuess: function() {
     let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
     let parsedGuess = parseInt(guess)
-
       if(parsedGuess >= this.smallestNum && parsedGuess <= this.biggestNum) {
       return parsedGuess
     }
+  },
+  render: function(lastGuess, secret, totalGuessAmount) {
+    if(lastGuess === secret) {
+      alert("Congrats! You guessed the number in " + totalGuessAmount + " guesses")
+    } else if(lastGuess > secret) {
+      alert("Your guess is too high!")
+    } else {
+      alert("Your guess is too low!")
+    }
   }
-  
+
 }
 
 game.play()
+
