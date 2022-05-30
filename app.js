c3129293c0fa3cf584467a8122173cd70c602e1d
Jesse Meirink <jesse@meirink.com>
2022-05-30 11:15:41 -0500

    add array.join method and complete all steps in project

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const game = {
       do {
         this.prevGuesses.push(this.getGuess())
         this.render(this.getGuess(), this.secretNum, this.prevGuesses.length)
+        
+        // Was using this to start the other 'commented out' render method
+        // this.render() 
       } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
   },
   getGuess: function() {
@@ -20,13 +23,25 @@ const game = {
       return parsedGuess
     }
   },
-  render: function(lastGuess, secret, totalGuessAmount) {
+
+  // Not sure why the code block below isn't working
+  // render: function() {
+  //   if(this.getGuess() === this.secretNum) {
+  //     alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses`)
+  //   } else if(lastGuess > secret) {
+  //     alert(`Your guess is too high! Previous guesses: ${this.prevGuesses.join()}`)
+  //   } else {
+  //     alert(`Your guess is too low! Previous guesses: ${this.prevGuesses.join()}`)
+  //   }
+  // }
+
+  render: function(lastGuess, secret, numOfGuesses) {
     if(lastGuess === secret) {
-      alert("Congrats! You guessed the number in " + totalGuessAmount + " guesses")
+      alert("Congrats! You guessed the number in " + numOfGuesses + " guesses")
     } else if(lastGuess > secret) {
-      alert("Your guess is too high!")
+      alert(`Your guess is too high! Previous guesses: ${this.prevGuesses.join()}`)
     } else {
-      alert("Your guess is too low!")
+      alert(`Your guess is too low! Previous guesses: ${this.prevGuesses.join()}`)
     }
   }
 
