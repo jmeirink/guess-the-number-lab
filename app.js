cd18e9ddd71d0a5d92f859745398578b83d3fb4f
Jesse Meirink <jesse@meirink.com>
2022-06-14 18:03:30 -0500

    fix minor bug and refactor game code

--- app.js
+++ app.js
@@ -1,51 +1,40 @@
+// We can always refer to the most recent guess with:
+// this.prevGuesses[this.prevGuesses.length - 1] 
+
+// Remember, the this keyword will let us access methods or properties of the parent object (game)
+
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
+  biggestNum: 10,
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
-  play: function() {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-
-      do {
-        this.prevGuesses.push(this.getGuess())
-        this.render(this.getGuess(), this.secretNum, this.prevGuesses.length)
-        
-        // Was using this to start the other 'commented out' render method
-        // this.render() 
-      } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
+  play: function () {
+    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    do {
+      this.prevGuesses.push(this.getGuess())
+      this.render()
+    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
   },
-  getGuess: function() {
-    let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
-    let parsedGuess = parseInt(guess)
-      if(parsedGuess >= this.smallestNum && parsedGuess <= this.biggestNum) {
-      return parsedGuess
+  getGuess: function () {
+    let guess // initialize guess variable
+    // while the guess is invalid (not a number & not within the set range)...
+    while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
+      // continue to prompt the user to enter in a new guess
+      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
     }
+    return guess // return the valid input
   },
-
-  // Not sure why the code block below isn't working
-  // render: function() {
-  //   if(this.getGuess() === this.secretNum) {
-  //     alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses`)
-  //   } else if(lastGuess > secret) {
-  //     alert(`Your guess is too high! Previous guesses: ${this.prevGuesses.join()}`)
-  //   } else {
-  //     alert(`Your guess is too low! Previous guesses: ${this.prevGuesses.join()}`)
-  //   }
-  // }
-
-  render: function(lastGuess, secret, numOfGuesses) {
-    if(lastGuess === secret) {
-      alert("Congrats! You guessed the number in " + numOfGuesses + " guesses")
-    } else if(lastGuess > secret) {
+  render: function () {
+    // if the most recent guess matches the secret num...
+    if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+      alert("Congrats! You guessed the number in " + this.prevGuesses.length + " guesses")
+    } else if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum) {
       alert(`Your guess is too high! Previous guesses: ${this.prevGuesses.join()}`)
     } else {
       alert(`Your guess is too low! Previous guesses: ${this.prevGuesses.join()}`)
     }
   }
-
 }
 
 game.play()
-
